2672d716989641cd22fdf8bcd9bcf1da14abdd2c
JC <chenjd98@163.com>
2021-05-09 16:59:04 -0400

    f

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import re
-import num2words
+from num2words import num2words
 
 LETTER_LIST = ['<sos>', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', \
          'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', "'", '.', '_', '+', ' ', '<eos>']
